f3c0a68bcf71999326840d89f7b56709c2311994
Marco Fracassi <fracassi.marco@gmail.com>
2020-01-10 09:12:21 +0100

    Moved templates from extensions

--- src/main/kotlin/ResponseExtensions.kt
+++ src/main/kotlin/ResponseExtensions.kt
@@ -1,9 +1,6 @@
+import Template.contentOf
 import daikon.Response
-import freemarker.template.Configuration
-import freemarker.template.Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS
 import org.eclipse.jetty.http.MimeTypes
-import java.io.File
-import java.io.StringWriter
 
 private const val DEFAULT_TEMPLATE_FOLDER = "/templates"
 
@@ -14,14 +11,4 @@ fun Response.render(name: String, model: HashMap<String, Any> = hashMapOf(), fol
 fun Response.html(name: String, model: HashMap<String, Any> = hashMapOf(), folder: String = DEFAULT_TEMPLATE_FOLDER) {
     this.type(MimeTypes.Type.TEXT_HTML_UTF_8.asString())
     this.render(name, model, folder)
-}
-
-private fun contentOf(name: String, model: HashMap<String, Any>, folder: String): String {
-    val configuration = Configuration(DEFAULT_INCOMPATIBLE_IMPROVEMENTS)
-    configuration.setClassForTemplateLoading(Response::class.java, folder)
-
-    StringWriter().use {
-        configuration.getTemplate("$name.ftl").process(model, it)
-        return it.toString()
-    }
 }
---
+++ src/main/kotlin/Template.kt
@@ -0,0 +1,16 @@
+import daikon.Response
+import freemarker.template.Configuration
+import java.io.StringWriter
+
+object Template {
+    fun contentOf(name: String, model: HashMap<String, Any>, folder: String): String {
+        val configuration =
+            Configuration(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS)
+        configuration.setClassForTemplateLoading(Response::class.java, folder)
+
+        StringWriter().use {
+            configuration.getTemplate("$name.ftl").process(model, it)
+            return it.toString()
+        }
+    }
+}
