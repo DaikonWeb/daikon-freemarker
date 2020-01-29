27e375aacd80f2c597c5fe6fda044056f5afc509
Alessio Coser <alessio.coser@xpeppers.com>
2020-01-29 11:41:21 +0100

    fixes auto-deploy script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,9 +3,11 @@ PROJECT_DIR=$(cd $(dirname $0)/..; pwd -P)
 PROJECT_NAME=$(basename $PROJECT_DIR)
 TAG=$1
 
-sed -ie "s|'com.github.DaikonWeb:daikon:.*'|'com.github.DaikonWeb:daikon:${TAG}'|g" ${PROJECT_DIR}/build.gradle
-sed -ie "s|'com.github.DaikonWeb:${PROJECT_NAME}:.*'|'com.github.DaikonWeb:${PROJECT_NAME}:${TAG}'|g" ${PROJECT_DIR}/README.md
-sed -ie "s|<version>.*</version>|<version>${TAG}</version>|g" ${PROJECT_DIR}/README.md
+cd ${PROJECT_DIR}
+
+sed -ie "s|'com.github.DaikonWeb:daikon:.*'|'com.github.DaikonWeb:daikon:${TAG}'|g" build.gradle
+sed -ie "s|'com.github.DaikonWeb:${PROJECT_NAME}:.*'|'com.github.DaikonWeb:${PROJECT_NAME}:${TAG}'|g" README.md
+sed -ie "s|<version>.*</version>|<version>${TAG}</version>|g" README.md
 
 git commit -am "Release ${TAG}"
 git tag $TAG
