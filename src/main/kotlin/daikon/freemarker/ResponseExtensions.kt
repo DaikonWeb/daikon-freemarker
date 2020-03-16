304fc6b5b614490b76849ce9e403a7337de96810
Alessio Coser <alessio.coser@xpeppers.com>
2020-03-16 17:11:38 +0100

    removes jetty dependency

--- src/main/kotlin/daikon/freemarker/ResponseExtensions.kt
+++ src/main/kotlin/daikon/freemarker/ResponseExtensions.kt
@@ -2,13 +2,12 @@ package daikon.freemarker
 
 import daikon.core.Response
 import daikon.freemarker.Template.DEFAULT_TEMPLATE_FOLDER
-import org.eclipse.jetty.http.MimeTypes
 
 fun Response.render(name: String, model: HashMap<String, Any> = hashMapOf(), folder: String = DEFAULT_TEMPLATE_FOLDER) {
     write(Template.render(name, model, folder))
 }
 
 fun Response.html(name: String, model: HashMap<String, Any> = hashMapOf(), folder: String = DEFAULT_TEMPLATE_FOLDER) {
-    type(MimeTypes.Type.TEXT_HTML_UTF_8.asString())
+    type("text/html;charset=utf-8")
     render(name, model, folder)
 }
